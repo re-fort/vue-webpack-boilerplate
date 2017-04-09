fd7eb0c306222f718d9df0f3836db5e00f1be711
re-fort <jeffpko@gmail.com>
2017-04-10 03:12:50 +0900

    :+1: Fix public path

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -83,7 +83,7 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#eval'
-  module.exports.output.publicPath = `/${_dist}/`
+  module.exports.output.publicPath = `/${_project}/${_dist}/`
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
