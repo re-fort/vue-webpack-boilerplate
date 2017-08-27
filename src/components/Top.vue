d81184106d09d6ff2588cef87aa5ca9ede59eca3
re-fort <jeffpko@gmail.com>
2017-08-27 19:39:14 +0900

    :+1: Show loading when waiting the request's result

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,7 +2,7 @@
   section.section
     .columns
       .column.is-3.is-offset-2(v-for="button in buttons")
-        router-link.button.selection(:to="button.url")
+        a.button.selection(:class="{ 'is-loading': isLoading && active === button.name }", @click="push(button)")
           span.icon
             i.fa(v-bind:class="button.icon")
           span {{ button.text }}
@@ -15,17 +15,28 @@ export default {
     return {
       buttons: [
         {
+          name: 'search',
           text: 'Search',
           icon: 'fa-search',
           url: '/search',
         },
         {
+          name: 'user',
           text: 'My Page',
           icon: 'fa-user',
           url: '/user',
         }
-      ]
+      ],
+      active: '',
+      isLoading: false,
     }
+  },
+  methods: {
+    push(button) {
+      this.active = button.name
+      this.isLoading = true
+      this.$router.push(button.url)
+    },
   },
 }
 </script>
