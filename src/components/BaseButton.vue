d76935d281c7ef597b25c73dd4acf598e9b51f88
re-fort <jeffpko@gmail.com>
2018-02-21 00:28:16 +0900

    :up: Upgrade eslint-plugin-vue (#48)
    * :up: Update dependency
    
    * :bulb: Fix glob pattern
    
    * :dress: Linting
    
    * :dress: Linting
    
    ref: https://github.com/vuejs/eslint-plugin-vue/blob/9da315ca07fe39d1e3d06c5328df42bf3767ecd6/docs/rules/require-valid-default-prop.md

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -14,20 +14,22 @@
     props: {
       classes: {
         type: Array,
+        default: () => [],
       },
       disabled: {
         type: Boolean,
         default: false,
       },
       value: {
+        type: [String, Number, Boolean, Object, Array],
         default: '',
       },
     },
     methods: {
       onClick() {
         this.$emit('onClick', this.value)
-      }
-    }
+      },
+    },
   }
 </script>
 
